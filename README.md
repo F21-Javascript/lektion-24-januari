f1690fe8f0ef934cc0e71a3604d8e0699c67ff8e
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-01-24 10:26:37 +0100

    Added code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# lektion-24-januari
+# Lektion 24 januari
+
+## Övningar
+
+DOM-bootcamp: https://gist.github.com/zocom-christoffer-wallenberg/e3649c00f51d7ce3176c0036a8bf21fd
+
+Till ovanstående övning använd även **sinus-minishop.zip** som du hittar i detta repo.
+
+## Artiklar
+
+## Videor
+
+## Inspelningar
---
+++ shopping-cart-exercise/js/index.js
@@ -0,0 +1,36 @@
+let products = document.getElementsByTagName('button');
+let shoppingCart = [];
+console.log(products);
+
+function updateCart() {
+    document.getElementById('productsInCart').innerHTML = shoppingCart.length;
+}
+
+function listProductsInCart() {
+    let cartProducts = '';
+
+    for(let i = 0; i < shoppingCart.length; i++) {
+        cartProducts += `<li><span class="product-title">Titel: </span> ${shoppingCart[i]} </li>`;
+    }
+
+    document.getElementById('products').innerHTML = cartProducts;
+}
+
+for(let i = 0; i < products.length; i++) {
+    console.log(products[i]);
+    products[i].addEventListener('click', (event) => {
+        console.log('Du klickade på en knapp', event);
+        let product = event.target.parentNode.getAttribute('data-product');
+        console.log('Du har valt ', product);
+        shoppingCart.push(product);
+        console.log(shoppingCart);
+
+        updateCart();
+        listProductsInCart();
+    });
+}
+
+document.getElementById('open-cart').addEventListener('click', () => {
+    document.getElementById('cart').classList.toggle('hide');
+    listProductsInCart();
+});
